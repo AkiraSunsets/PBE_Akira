9a7116be9fc1a5b479c98a588b5864167f978eb3
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-03 23:06:40 -0300

    Update python file2.py

--- python file2.py
+++ python file2.py
@@ -227,7 +227,43 @@ else:
 
 #18
 
+expressao = input("Digite uma expressão matemática: ")
+
+resultado = eval(expressao)
+
+print(f"O resultado da expressão é: {resultado}")
+
+#19
+
 import re
 
-cpf = input("Digite seu CPF")
-padrao = r"^"
+cpf = input("Digite o número do CPF no formato (XXX.XXX.XXX-XX): \n")
+padrao_cpf = r"\d{3}\.\d{3}\.\d{3}\-\d{2}"
+formato_valido = re.match(padrao_cpf, cpf)
+
+if formato_valido:
+    soma_first = (
+        (int(cpf[0]) * 10) + (int(cpf[1]) * 9) + (int(cpf[2]) * 8) + (int(cpf[4]) * 7) +
+        (int(cpf[5]) * 6)  + (int(cpf[6]) * 5) + (int(cpf[8]) * 4) + (int(cpf[9]) * 3) + (int(cpf[10]) * 2)) % 11
+
+    soma_seccond = (
+        (int(cpf[0]) * 11) + (int(cpf[1]) * 10) + (int(cpf[2]) * 9) + (int(cpf[4]) * 8) +
+        (int(cpf[5]) * 7)  + (int(cpf[6]) * 6)  + (int(cpf[8]) * 5) +
+        (int(cpf[9]) * 4)  + (int(cpf[10]) * 3) + (int(cpf[12]) * 2)) % 11
+
+    if soma_first >= 2:
+        digito1 = 11 - soma_first
+    else:
+        digito1 = 0
+
+    if soma_seccond >= 2:
+        digito2 = 11 - soma_seccond
+    else:
+        digito2 = 0
+
+    if int(cpf[12]) == digito1 and int(cpf[13]) == digito2:
+        print(f"O CPF {cpf} é válido!")
+    else:
+        print(f"O CPF {cpf} é inválido!")
+else:
+    print("O CPF não foi digitado corretamente! Tente novamente.")
